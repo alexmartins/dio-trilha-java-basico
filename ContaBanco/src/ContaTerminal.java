e1567d3d569b3ba214ed5aab319228f573ff1d8a
Alex Martins de Souza <alex@alexmartins.com>
2025-04-15 21:05:49 -0300

    feat: incluindo interação de prompt na aplicação.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Desafio:
+        // https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        String numero = scanner.nextLine();
+
+        System.out.println("Por favor, digite o Saldo da Conta: ");
+        String saldo = scanner.nextLine();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        // Fechar o scanner para evitar vazamento de recursos
+        scanner.close();
+
     }
 }
